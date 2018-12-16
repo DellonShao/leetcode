6fac53e5bd52551d5963befe1618535a3f0dff19
DellonShao <14301102@bjtu.edu.cn>
2018-12-16 16:03:12 +0800

    2018-12-16

---
+++ src/_114_FlattenBinaryTreeToLinkedList/MainClass.java
@@ -0,0 +1,122 @@
+package _114_FlattenBinaryTreeToLinkedList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        dfs(root);
+    }
+
+    private TreeNode dfs(TreeNode root){
+        TreeNode tmpLeft = root.left;
+        TreeNode tmpRight = root.right;
+        TreeNode last = root;
+        if(tmpLeft != null){
+            root.left = null;
+            root.right = tmpLeft;
+            //last = tmpLeft;
+            last = dfs(tmpLeft);
+        }
+        if (tmpRight != null) {
+            last.left = null;
+            last.right = tmpRight;
+            //last = tmpRight;
+            last = dfs(tmpRight);
+        }
+        return last;
+    }
+}
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+
+        String output = "";
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (node == null) {
+                output += "null, ";
+                continue;
+            }
+
+            output += String.valueOf(node.val) + ", ";
+            nodeQueue.add(node.left);
+            nodeQueue.add(node.right);
+        }
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            new Solution().flatten(root);
+            String out = treeNodeToString(root);
+
+            System.out.print(out);
+        }
+    }
+}
---
+++ src/_129_SumRootToLeafNumbers/MainClass.java
@@ -0,0 +1,96 @@
+package _129_SumRootToLeafNumbers;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    private int sum = 0;
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        dfs(root, 0);
+        return sum;
+    }
+
+    private void dfs(TreeNode root, int num){
+        if(root.left == null && root.right == null){
+            sum += num * 10 + root.val;
+            return;
+        }
+        if(root.left != null)
+            dfs(root.left, num * 10 + root.val);
+        if(root.right != null)
+            dfs(root.right, num * 10 + root.val);
+    }
+}
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+
+            int ret = new Solution().sumNumbers(root);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
