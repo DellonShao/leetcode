f513df6b71e7fa442909f14d571479dd2b0654b4
DellonShao <14301102@bjtu.edu.cn>
2019-03-22 22:55:23 +0800

    2019-03-22

---
+++ src/_230_KthSmallestElementInaBST/MainClass.java
@@ -0,0 +1,104 @@
+package _230_KthSmallestElementInaBST;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    int pos = 0;
+    int val = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root,k);
+        return val;
+    }
+    public void inOrder(TreeNode root,int k){
+        if(root == null){
+            return;
+        }
+        if(root.left != null){
+            inOrder(root.left, k);
+        }
+        pos = pos + 1;
+        if(pos == k) {
+            val = root.val;
+            return ;
+        }
+        if(root.right != null && k > pos){
+            inOrder(root.right, k);
+        }
+    }
+}
+
+public class MainClass {
+    public static TreeNode stringToTreeNode(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+            return null;
+        }
+
+        String[] parts = input.split(",");
+        String item = parts[0];
+        TreeNode root = new TreeNode(Integer.parseInt(item));
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        nodeQueue.add(root);
+
+        int index = 1;
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int leftNumber = Integer.parseInt(item);
+                node.left = new TreeNode(leftNumber);
+                nodeQueue.add(node.left);
+            }
+
+            if (index == parts.length) {
+                break;
+            }
+
+            item = parts[index++];
+            item = item.trim();
+            if (!item.equals("null")) {
+                int rightNumber = Integer.parseInt(item);
+                node.right = new TreeNode(rightNumber);
+                nodeQueue.add(node.right);
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            TreeNode root = stringToTreeNode(line);
+            line = in.readLine();
+            int k = Integer.parseInt(line);
+
+            int ret = new Solution().kthSmallest(root, k);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
